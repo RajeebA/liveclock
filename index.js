6b5a0e256e41c86bc9b3732d8efc8cb6c35be15d
RajeebAK <rajeeb@riafy.me>
2021-01-04 10:14:14 +0530

    bug fixing

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ setInterval(() => {
   let hh = day.getHours() * 30;
   let mm = day.getMinutes() * deg;
   let ss = day.getSeconds() * deg;
-  console.log(hh + mm / 2);
-  hr.style.transform = `rotateZ(${hh + mm / 2}deg)`;
+  let hours = (360 / 100) * ((hh / 12) * 100);
+  hours = hh + mm / 12;
+
+  hr.style.transform = `rotateZ(${hours}deg)`;
   mn.style.transform = `rotateZ(${mm}deg)`;
   sc.style.transform = `rotateZ(${ss}deg)`;
 });
